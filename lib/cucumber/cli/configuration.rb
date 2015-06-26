e9d16efe903245d7fe583ba72f68fd9ec41e594b
Steve Tooke <steve@boxjump.co.uk>
2015-06-26 16:14:18 +0100

    Move responsibility for registering to the configuration

--- lib/cucumber/cli/configuration.rb
+++ lib/cucumber/cli/configuration.rb
@@ -181,6 +181,10 @@ module Cucumber
         @options[:snippet_generators] ||= []
       end
 
+      def register_snippet_generator(generator)
+        snippet_generators << generator
+      end
+
       private
 
       def default_features_paths

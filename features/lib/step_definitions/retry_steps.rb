cca5aa034b327ea8af8c07bfd2db7f0c4beabcd0
Matt Wynne <matt@mattwynne.net>
2016-09-02 23:00:13 +0100

    Revert "Polish off retry"

--- features/lib/step_definitions/retry_steps.rb
+++ features/lib/step_definitions/retry_steps.rb
@@ -1,54 +1,35 @@
-Given /^a scenario "([^\"]*)" that fails once, then passes$/ do |full_name|
-  name = snake_case(full_name)
+Given /^a scenario "([^\"]*)" that fails once, then passes$/ do |name|
   write_file "features/#{name}.feature",
   <<-FEATURE
-  Feature: #{full_name} feature
-    Scenario: #{full_name}
+  Feature: #{name}
+    Scenario: #{name}
       Given it fails once, then passes
   FEATURE
 
   write_file "features/step_defnitions/#{name}_steps.rb",
   <<-STEPS
   Given(/^it fails once, then passes$/) do
-    $#{name} += 1
-    expect($#{name}).to be > 1
+    $#{name.downcase} ||= 0
+    $#{name.downcase} += 1
+    expect($#{name.downcase}).to eql 2
   end
   STEPS
-
-  write_file "features/support/#{name}_init.rb",
-  <<-INIT
-  $#{name} = 0
-  INIT
 end
 
-Given /^a scenario "([^\"]*)" that fails twice, then passes$/ do |full_name|
-  name = snake_case(full_name)
+Given /^a scenario "([^\"]*)" that fails twice, then passes$/ do |name|
   write_file "features/#{name}.feature",
   <<-FEATURE
-  Feature: #{full_name} feature
-    Scenario: #{full_name}
+  Feature: #{name}
+    Scenario: #{name}
       Given it fails twice, then passes
   FEATURE
 
   write_file "features/step_definitions/#{name}_steps.rb",
   <<-STEPS
   Given(/^it fails twice, then passes$/) do
-    $#{name} ||= 0
-    $#{name} += 1
-    expect($#{name}).to be > 2
+    $#{name.downcase} ||= 0
+    $#{name.downcase} += 1
+    expect($#{name.downcase}).to eql 3
   end
   STEPS
-
-  write_file "features/support/#{name}_init.rb",
-  <<-INIT
-  $#{name} = 0
-  INIT
 end
-
-module SnakeCase
-  def snake_case(name)
-    name.downcase.gsub(/\W/, '_')
-  end
-end
-
-World(SnakeCase)
